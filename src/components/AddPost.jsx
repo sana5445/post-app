cdacc3a49cd07a80037190c3737062134a1f73f5
sana54 <sanahaiza5445@gmail.com>
2025-07-14 21:01:40 +0530

    project created

---
+++ src/components/AddPost.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import PostNavbar from './PostNavbar'
+
+const AddPost = () => {
+  return (
+    <div>
+        <PostNavbar/>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row">
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <div className="label form-label">userId</div>
+                            <input type="number" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                             <div className="label form-label">id</div>
+                            <input type="number" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                             <div className="label form-label">title</div>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                             <div className="label form-label">body</div>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <button className="btn btn success">SUBMIT</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AddPost
